c514f8fcb1cd76ca8e5eba6b5da0afc3a148da7b
mraniket404 <aniketgosavi471@gmail.com>
2026-09-12 13:30:22 +0530

    py syntax

---
+++ 02-python-syntax/syntax_examples.py
@@ -0,0 +1,17 @@
+# Python Syntax Practice
+
+print("Hello, World!")
+
+# Variable examples
+x = 5
+y = "Hello, Python!"
+
+print(x)
+print(y)
+
+# Indentation example
+if 5 > 2:
+    print("Five is greater than two!")
+
+# This is a comment
+print("Python comments start with #")
