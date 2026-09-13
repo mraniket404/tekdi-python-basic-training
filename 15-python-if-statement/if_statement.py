04320766eef3ec145b2f4fca6a95ba626532b8ed
mraniket404 <aniketgosavi471@gmail.com>
2026-09-13 19:54:49 +0530

    Add Python if statement examples

---
+++ 15-python-if-statement/if_statement.py
@@ -0,0 +1,49 @@
+# Python If Statement
+
+# Basic if statement
+a = 33
+b = 200
+
+if b > a:
+    print("b is greater than a")
+
+
+# Check positive number
+number = 15
+
+if number > 0:
+    print("The number is positive")
+
+
+# Multiple statements
+age = 20
+
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You have full legal rights")
+
+
+# Boolean variable
+is_logged_in = True
+
+if is_logged_in:
+    print("Welcome back!")
+
+
+# False value
+number = 0
+
+if number:
+    print("This will not print")
+
+
+# Non-empty string is True
+message = "Hello"
+
+if message:
+    print("Message is available")
+
+
+# Exercise
+print(5 > 3)
