5b9c325a87d2696196505e25c0e149f118a0bb00
mraniket404 <aniketgosavi471@gmail.com>
2026-09-13 17:51:40 +0530

    Add Python casting examples

---
+++ 07-python-casting/casting.py
@@ -0,0 +1,33 @@
+# Python Casting
+
+# Integer casting
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+# Float casting
+a = float(1)
+b = float(2.8)
+c = float("3")
+d = float("4.2")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# String casting
+p = str("s1")
+q = str(2)
+r = str(3.0)
+
+print(p)
+print(q)
+print(r)
+
+# Exercise
+print(int(35.88))
