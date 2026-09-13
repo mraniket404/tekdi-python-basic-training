85b1c837b22af38d6d2fdb3d021d4c6d8fbdcd0d
mraniket404 <aniketgosavi471@gmail.com>
2026-09-13 17:47:15 +0530

    Add Python numbers examples

---
+++ 06-python-numbers/numbers.py
@@ -0,0 +1,36 @@
+# Python Numbers
+
+# Integer
+x = 10
+y = -25
+
+print(x)
+print(type(x))
+
+# Float
+a = 10.5
+b = -35.59
+
+print(a)
+print(type(a))
+
+# Complex
+c = 3 + 5j
+
+print(c)
+print(type(c))
+
+# Type conversion
+num = 10
+
+float_num = float(num)
+complex_num = complex(num)
+
+print(float_num)
+print(complex_num)
+
+# Random number
+import random
+
+random_number = random.randrange(1, 10)
+print(random_number)
